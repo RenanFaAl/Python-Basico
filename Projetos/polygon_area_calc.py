8fca52db85fa5841293778829a2021a121099ad1
Renan Fabiani <renanfabianialmeida@gmail.com>
2026-01-30 11:04:13 -0300

    Polygon area calculator Project

---
+++ Projetos/polygon_area_calc.py
@@ -0,0 +1,64 @@
+import math
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+
+    def set_width(self, width):
+        self.width = width
+    
+    
+    def set_height(self, height):
+        self.height = height
+    
+    
+    def get_area(self):
+        return self.height * self.width
+    
+    
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    
+    
+    def  get_diagonal(self):
+        return math.sqrt(self.width ** 2 + self.height ** 2)
+    
+    
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        picture = ''
+        for size in range(self.height):
+            picture += '*' * self.width + '\n'
+        return picture
+    
+    
+    def get_amount_inside(self, shape):
+        return self.get_area() // shape.get_area()
+
+    
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def __str__(self):
+        return f'Square(side={self.width})'
+
+    def set_side(self, side):
+        self.height = side
+        self.width = side
+
+    def set_width(self, width):
+        return self.set_side(width)
+    
+    def set_height(self, height):
+        return self.set_side(height)
+
+
+rect = Rectangle(10,5)
+
+print(rect.get_picture())
